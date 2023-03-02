d8d57cb55a8eab8ded57db00ab5f12dd5417f2be
kpcyrd <git@rxv.cc>
2023-03-02 17:19:07 +0100

    Add more debian signing keys

--- contrib/debian-snapshot-importer.sh
+++ contrib/debian-snapshot-importer.sh
@@ -3,11 +3,13 @@ set -u
 
 curl -sSf 'https://snapshot.notset.fr/by-timestamp/debian.txt' | sort -r | \
 while read -r snap; do
-    echo "[+] Importing $snap..."
-    base_url="https://snapshot.notset.fr/archive/debian/$snap/dists/bullseye/"
-    curl -sSf "$base_url/InRelease" | apt-swarm import
-    apt-swarm plumbing attach-sig \
-        <(curl -sSf "$base_url/Release") \
-        <(curl -sSf "$base_url/Release.gpg") \
-        | apt-swarm import
+    for dist in bookworm bullseye buster sid unstable; do
+        echo "[+] Importing $snap... ($dist)"
+        base_url="https://snapshot.notset.fr/archive/debian/$snap/dists/$dist/"
+        curl -sSf "$base_url/InRelease" | apt-swarm import
+        apt-swarm plumbing attach-sig \
+            <(curl -sSf "$base_url/Release") \
+            <(curl -sSf "$base_url/Release.gpg") \
+            | apt-swarm import
+    done
 done
