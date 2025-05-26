3b5185262c7df380cbe7ebd7d687be7edc861d35
kpcyrd <git@rxv.cc>
2025-05-26 22:24:00 +0200

    Allow fetching a specific repository selectively

--- src/fetch.rs
+++ src/fetch.rs
@@ -58,9 +58,12 @@ pub async fn fetch_updates<D: DatabaseClient>(
     concurrency: Option<usize>,
     repositories: Vec<Repository>,
     proxy: Option<SocketAddr>,
+    fingerprints: &[sequoia_openpgp::Fingerprint],
 ) -> Result<()> {
     let concurrency = concurrency.unwrap_or(DEFAULT_CONCURRENCY);
-    let mut queue = repositories.into_iter();
+    let mut queue = repositories.into_iter().filter(|repository| {
+        fingerprints.is_empty() || repository.contains_fingerprint(fingerprints)
+    });
     let mut pool = JoinSet::new();
     let client = client(proxy)?;
 
--- src/p2p/fetch.rs
+++ src/p2p/fetch.rs
@@ -64,7 +64,7 @@ pub async fn spawn_fetch_timer<D: DatabaseClient>(
         p2p::random_jitter(p2p::FETCH_INTERVAL_JITTER).await;
         info!("Fetch timer has started");
         if let Err(err) =
-            fetch::fetch_updates(db, keyring.clone(), None, repositories.clone(), proxy).await
+            fetch::fetch_updates(db, keyring.clone(), None, repositories.clone(), proxy, &[]).await
         {
             error!("Fetch timer has crashed: {err:#}");
         } else {
